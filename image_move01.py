ae0e21c3bb0a609e482100e32e5bec6f779a3c5b
kosangmin <kosangmin822@gmail.com>
2020-12-10 15:38:49 +0900

    transform

---
+++ image_move01.py
@@ -0,0 +1,18 @@
+import numpy as np
+import cv2
+
+
+def transform():
+    img = cv2.imread('images/4.jpg')
+    h, w = img.shape[:2]
+
+    M = np.float32([[1, 0, 100], [0, 1, 50]])
+
+    img2 = cv2.warpAffine(img, M, (w, h))
+    cv2.imshow('shift image', img2)
+
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+
+
+transform()
